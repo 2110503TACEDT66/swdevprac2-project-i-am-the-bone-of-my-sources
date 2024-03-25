8893062aea171ae548bc8aac0933e3e2b37909a0
Patipon Sanor <6633134721@student.chula.ac.th>
2024-03-26 02:16:32 +0700

    Revert "Fix type error"
    This reverts commit 4a2e0c6605a8828abb88364160e2be118e2f5817.

--- src/libs/getUserProfile.tsx
+++ src/libs/getUserProfile.tsx
@@ -1,4 +1,4 @@
-export default async function getUserProfile(token: string | undefined) {
+export default async function getUserProfile(token: string) {
 
     const response = await fetch(`${process.env.BACKEND_URL}/api/v1/auth/me`, {
         method: "GET",
