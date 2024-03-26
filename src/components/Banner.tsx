b67772f6dc0140cc8d14a50447c270fd9f456198
Taihenc <51960471+Taihenc@users.noreply.github.com>
2024-03-26 11:24:34 +0700

    add/update banner animation

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,11 +1,26 @@
 'use client';
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Banner = () => {
   const [isMounted, setIsMounted] = useState(false);
+  const textRef = useRef<HTMLDivElement>(null);
   useEffect(() => {
     setIsMounted(true);
+    addEventListener("scroll", () => {
+      if (window.scrollY > window.innerHeight * 0.7) {
+        setIsMounted(false);
+      } else {
+        setIsMounted(true);
+      }
+    });
+    addEventListener("mousemove", (e) => {
+      const x = e.clientX / 70;
+      const y = e.clientY / 70;
+      if (textRef.current) {
+        textRef.current.style.transform = `translate(${x}px, ${y}px)`;
+      }
+    });
   }, []);
   return (
     <div className="h-[100vh] w-[100vw]">
@@ -19,7 +34,7 @@ const Banner = () => {
       />
       {
         isMounted &&
-        <div className="absolute md:top-[50%] md:left-[15%] leading-[80px] select-none text-white fade-in-then-text-glowing">
+        <div ref={textRef} className="absolute md:top-[50%] md:left-[15%] leading-[80px] select-none text-white fade-in-then-text-glowing">
           <div className="text-[100px] font-extrabold opacity-85">GO CAMPING</div>
           <div className="text-[100px] font-extrabold filter blur-[2px] scale-y-[-1] opacity-85">GO CAMPING</div>
         </div>
