6459f3d66e84bf42cb242745a041480bcf4102c7
Patipon Sanor <6633134721@student.chula.ac.th>
2024-03-26 11:13:01 +0700

    UpdateBooking feature

--- src/app/bookings/[id]/page.tsx
+++ src/app/bookings/[id]/page.tsx
@@ -1,6 +1,7 @@
 import BookingDetailCard from "@/components/BookingDetailCard";
 import { authOptions } from "@/libs/authOptions";
 import { getBooking } from "@/libs/getBooking";
+import getCampgrounds from "@/libs/getCampgrounds";
 import getUserProfile from "@/libs/getUserProfile2";
 import { getServerSession } from "next-auth";
 
@@ -8,13 +9,14 @@ export default async function BookingDetailPage({ params }: { params: { id: stri
 
     const session = await getServerSession(authOptions);
     const bookingItemResponse = await getBooking(session?.user.token, params.id);
+    const campgroundJson = await getCampgrounds();
     const userResponse = await getUserProfile(session?.user.token);
     const showUser = userResponse.data.role === "admin";
 
     return (
         <main className="flex flex-row justify-center">
             {bookingItemResponse ?
-                <BookingDetailCard bookingItem={bookingItemResponse?.data} showUser={showUser} />
+                <BookingDetailCard bookingItem={bookingItemResponse?.data} showUser={showUser} campgroundsList={campgroundJson.data} />
                 : <div> Booking ID : {params.id} not found </div>
             }
         </main>
