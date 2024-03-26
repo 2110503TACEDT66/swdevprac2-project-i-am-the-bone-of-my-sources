4d6e450ce5f52567c79ffa6c2bc7273848e931a8
Patipon Sanor <6633134721@student.chula.ac.th>
2024-03-26 11:59:02 +0700

    Add ability to add booking

--- src/components/CampgroundsSelector.tsx
+++
@@ -1,9 +0,0 @@
-import { MenuItem, Select } from "@mui/material";
-
-export default function CampgroundsSelector() {
-  return (
-    <Select className="mx-2 h-16" variant="standard" >
-      <MenuItem value={"Campground_1"}>Campground_1</MenuItem>
-    </Select>
-  );
-}
